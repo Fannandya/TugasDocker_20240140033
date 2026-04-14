763658a89d12bc6cbb32803bfdbb6e5740636a23
Fannandya Sutan <sfannandya@gmail.com>
2026-04-14 09:45:57 +0700

    menambahkan function halaman login

--- src/main/java/com/tugas6/deploy/controller/UserController.java
+++ src/main/java/com/tugas6/deploy/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.tugas6.deploy.controller;
 
+import com.tugas6.deploy.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@controller
 public class UserController {
+    private static List<User> mahasiswaList = new ArrayList<>();
+
+    @GetMapping("/")
+    public String loginPage() {
+        return "login";
+    }
+
+
+
+
+
 }
