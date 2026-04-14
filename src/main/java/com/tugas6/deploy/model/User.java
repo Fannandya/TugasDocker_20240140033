6e0432d504e4a03b49a69b0e338d23ce813f5a34
Fannandya Sutan <sfannandya@gmail.com>
2026-04-14 09:36:05 +0700

    menambahkan function untuk menyimpan data mahasiswa

--- src/main/java/com/tugas6/deploy/model/User.java
+++ src/main/java/com/tugas6/deploy/model/User.java
@@ -1,4 +1,15 @@
 package com.tugas6.deploy.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
+    private String nama;
+    private String nim;
+    private String jenisKelamin;
 }
